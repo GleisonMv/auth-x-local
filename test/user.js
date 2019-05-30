7753ce2987598fdbd33cb70be74b238c55c28a1a
Gleison <gleisonmorgado@hotmail.com>
2019-05-30 07:48:48 -0400

    fix

--- test/user.js
+++ test/user.js
@@ -9,6 +9,7 @@ test('test headers', function (assert) {
   assert.notEqual(instance, undefined)
   assert.notEqual(instance.onUserCheck, undefined)
   instance.onUserCheck({}, function (err, isAuth) {
+    assert.error(err)
     assert.equal(isAuth, false)
     assert.end()
   })
@@ -22,6 +23,7 @@ test('test headers.authorization', function (assert) {
   assert.notEqual(instance, undefined)
   assert.notEqual(instance.onUserCheck, undefined)
   instance.onUserCheck({ headers: {} }, function (err, isAuth) {
+    assert.error(err)
     assert.equal(isAuth, false)
     assert.end()
   })

294b561453d90f056be97580cbcf28bfaab6a13c
Gleison <gleisonmorgado@hotmail.com>
2019-06-01 07:56:19 -0400

    fix types

--- types/auth-x-local.d.ts
+++ types/auth-x-local.d.ts
@@ -9,10 +9,10 @@ declare interface Option
 declare class Local extends auth.strategy
 {
     constructor(config: Option)
-    auth(id: number, group: number, reply: any);
-    onError(request: any, reply: any, error: number);
-    onPermissionCheck(request: any, done: (err: Error, auth?: boolean) => void);
-    onUserCheck(request: any, permission: number, done: (err: Error, permission?: boolean) => void);
+    auth(id: number, group: number, reply: any): void;
+    onError(request: any, reply: any, error: number): void;
+    onPermissionCheck(request: any, done: (err: Error, auth?: boolean) => void): void;
+    onUserCheck(request: any, permission: number, done: (err: Error, permission?: boolean) => void): void;
 }
 
 export default Local
